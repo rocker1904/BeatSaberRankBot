3229d23c6544f5c8f5d4ea5937467c49faf4f584
rocker1904 <fly251211@gmail.com>
2019-07-07 12:20:29 +0100

    Add profile command

---
+++ commands/profile.js
@@ -0,0 +1,25 @@
+const scraper = require('../scraper.js');
+const database = require('../config.json').database;
+const Keyv = require('keyv');
+const db2 = new Keyv(`${database}`, { namespace: 'discord' });
+db2.on('error', err => console.error('Keyv connection error:', err));
+
+module.exports = {
+	name: 'profile',
+	description: 'Returns your profile.',
+	args: false,
+	async execute(message) {
+
+		// If user is in database, return associated Scoresaber profile
+		const scoresaber = await db2.get(message.author.id).catch(err => {
+			console.log(err);
+		});
+
+		if (scoresaber == null) {
+			message.channel.send('You are not in the database.');
+			return;
+		}
+
+        message.channel.send(`Your ScoreSaber profile: https://scoresaber.com${scoresaber}`);
+	},
+};
